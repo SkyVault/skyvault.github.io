bf9884d51a281b77971f63821d69eb976949e067
Dustin Neumann <neumdu@csl02.cs.wallawalla.edu>
2018-10-16 13:51:59 -0700

    We have notable projects listed

---
+++ entries/test.md
@@ -0,0 +1,34 @@
+---
+date: 10-16-2018
+---
+
+# Testing the post compilation :)
+
+Lemon bars
+
+## Sub header for list
+1. This
+2. Is
+3. A
+4. List
+
+## Sub header for todo's
+- [ ] it would be nice if this worked
+- [ ] it would be nice if this worked
+- [ ] it would be nice if this worked
+- [ ] it would be nice if this worked
+
+## Sub header for source code
+```c
+#include <stdlib.h>
+struct V2 {
+    float x; float y;
+};
+
+int main() {
+    struct V2* myPoint = (struct V2*)malloc(sizeof(struct V2));
+    free(myPoint);
+    return 0;
+}
+```
+
--- gen.nim
+++ gen.nim
@@ -1,7 +1,32 @@
-import system, os
+import system, os, ospaths, osproc, strformat, json
 import templates
+import strutils
 
-proc index() : string = tmpli html"""
+import libcurl
+
+proc projectCard(name : string, url = "", desc = "") : string = tmpli html"""
+  <div class="ProjectCard">
+    <h4> $name </h4>
+    <h5> $desc </h5>
+    <a href="$url" target="_blank"> View </a>
+  </div>
+  """
+
+const Projects = [
+  ("Coral", "https://github.com/SkyVault/Coral", """
+  Coral is a simple, easy to use 2d game framework for the Nim programming language. Warning: Coral is under heavy development.
+  """),
+
+  ("SkyVault", "https://github.com/SkyVault/SkyVault", """
+    SkyVault is a 2d game developed in c++
+  """),
+
+  ("NimTiled", "https://github.com/SkyVault/nim-tiled", """
+  Tiled map loader for the nim programming language
+  """)
+]
+
+proc index(names : seq[string] = @[]) : string = tmpli html"""
   <html>
     <head>
       <title> Sky Vault </title>
@@ -11,8 +36,46 @@ proc index() : string = tmpli html"""
       <div id="Header">
         <h1> Sky Vault </h1>
       </div>
+
+      <div>
+        <div id="NotableProjects">
+          <h3> Notable projects </h3>
+          
+          <div id="Projects">
+            $for p in Projects {
+              $(projectCard(p[0], p[1], p[2]))
+            }
+          </div>
+        </div>
+
+        <div id="RecentBlogEntries">
+          <h3> Recent Blog Entries </h3>
+
+          <ul>
+            $for e in names {
+              <li> $e </li>
+            }
+          </ul>
+          <button> View all </button>
+        </div>
+      </div>
     <body>
   </html>
   """
 
-writeFile("index.html", index())
+# Get list of posts
+
+var entries = newSeq[string]()
+var names = newSeq[string]()
+
+for kind, entry in walkDir(getCurrentDir() & "/entries"):
+  case kind:
+  of pcFile:
+    let (dir, name, ext) = splitFile(entry)
+    if ext == ".md":
+      entries.add entry
+      names.add name
+      discard execShellCmd(&"pandoc {entry} -o {getCurrentDir() & \"/output/\" & name & \".html\"}")
+  else: discard
+
+writeFile("index.html", index names)
